02ad1c5ccb7d0db2e821ab56a27e0cd2c173de02
aShinde <ananta.shinde@honeywellcom>
2024-08-27 11:58:42 +0530

    27/08/24

---
+++ oops/oop_demo.py
@@ -0,0 +1,26 @@
+
+class Student:
+   __name = None
+   __rollNo = None
+   __address = None
+   __college = None
+   __branch = None
+   __email = None
+   __contact = None
+   __password = None
+   subjects = None
+
+   def login(self):
+       pass
+
+   def __update(self):
+       pass
+
+   def printDetails(self):
+       pass
+
+
+
+
+
+
